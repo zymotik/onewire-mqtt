d39e7b3ccdc5c0cc926a642c4ecc3be859cb4d1d
Ian Brown <email@ian-b.com>
2019-02-24 12:33:22 +0000

    removed old service code

--- service/service-details.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name:'onewire-mqtt',
-    description: 'A onewire to MQTT broker node module',
-    script: require('path').join(__dirname, '../index.js'),
- //   user: "vagrant",
- //   group: "vagrant",
-  };
